a9d14bae51777a779fef5943d30d7e72f534c364
Arjen Oosterkamp <mail@arjen.me>
2015-02-18 11:45:44 +0100

    Allow own model class to be defined

--- lib/scoped_serializer/collection_serializer.rb
+++ lib/scoped_serializer/collection_serializer.rb
@@ -4,12 +4,17 @@ module ScopedSerializer
     def initialize(*args)
       super
 
+      options = args.extract_options!
+
+      # Allow to define own model class
+      @model_class = options.delete(:model_class) || @array.klass
+
       # Configure root element
-      @options[:root] = default_root_key(@array.klass).pluralize if @options[:root].nil?
+      @options[:root] = default_root_key(@model_class).pluralize if @options[:root].nil?
     end
 
     def to_csv(options={})
-      attributes = ScopedSerializer.find_serializer_by_class(@array.klass)
+      attributes = ScopedSerializer.find_serializer_by_class(@model_class)
              .find_scope(options[:scope] || :default)
              .attributes
 
